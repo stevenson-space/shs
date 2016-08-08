bbf950cee4eb8948040b660405ff63a8f3a1aec0
Chaldron <moonsketch@me.com>
2016-08-08 13:45:21 -0500

    Rewrote CSS/JS + general site updates

--- js/bell.js
+++ js/bell.js
@@ -1,185 +1,145 @@
-var today;
-var minutes;
-var timer;
-$(function() { main(); });
-$(window).on("ontouchup", function()
+var minutes, timer;
+
+//Run main function on document load
+$(main);
+//Reset timer and rerun on regained
+//focus or a touch on mobile device
+$(window).on("ontouchup", "focus", function() 
 {
-	clearInterval(timer);
-	main();
-});
-$(window).focus(function()
-{ 
-	clearInterval(timer);
-	main();
-});
+    clearInterval(timer);
+    main();
+}); 
+
 
 function main()
 {
-	today = new Date();
-	minutes = today.getHours()*60 + today.getMinutes();
+    var today = new Date();
+    minutes = today.getHours() * 60 + today.getMinutes(); //minutes offset
 
-	var sdow = ["8.25.2015", "9.1.2015", "9.9.2015", "9.16.2015", "9.22.2015", "9.29.2015", "10.6.2015", "10.14.2015", "10.20.2015", "10.27.2015",
-	"11.3.2015", "11.10.2015", "11.17.2015", "11.24.2015", "12.1.2015", "12.8.2015", "12.15.2015", "1.5.2016", "1.26.2016", "2.2.2016",
-	"2.17.2016", "2.23.2016", "3.1.2016", "3.9.2016", "3.15.2016", "3.22.2016", "4.5.2016", "4.12.2016", "4.19.2016", "4.26.2016", "5.3.2016",
-	"5.10.2016", "5.17.2016", "5.24.2016"];
+    var date = getDateString(today)
 
-	var latearriv = ["8.27.2015", "12.10.2015", "9.3.2015", "9.11.2015", "9.17.2015", "10.22.2015", "11.19.2015", "1.28.2016", "2.18.2016", "3.17.2016", "4.21.2016"];
-	var actper = ["4.13.2016","5.16.2016"];
-	var pmasm = ["5.27.2016"];
-	var holidays = ["4.1.2016","6.6.2016","6.17.2016","6.24.2016","7.1.2016","7.4.2016","7.15.2016","7.22.2016","7.29.2016"];
-	
-	var finals = ["5.31.2016","6.1.2016","6.2.2016"];
-	var summer = ["6.7.2016","6.8.2016","6.9.2016","6.10.2016","6.13.2016","6.14.2016","6.15.2016","6.16.2016","6.20.2016","6.21.2016","6.22.2016","6.23.2016",
-	"6.27.2016","6.28.2016","6.29.2016","6.30.2016","7.5.2016","7.6.2016","7.7.2016","7.8.2016","7.11.2016","7.12.2016","7.13.2016","7.14.2016",
-	"7.18.2016","7.19.2016","7.20.2016","7.21.2016","7.25.2016","7.26.2016","7.27.2016","7.28.2016"];
-	
-	var date = (today.getMonth() + 1) + "." + today.getDate() + "." + today.getFullYear();
+    var display = {};
 
-	var display = {};
-	
-	if(!(today.getDay() % 6) || holidays.indexOf(date) != -1)
-	{
-		display.schedule = "No school";
-		display.period = "";
-		while(!(today.getDay() % 6) || holidays.indexOf(date) != -1){
-			today.setDate(today.getDate()+1);
-			date = (today.getMonth() + 1) + "." + today.getDate() + "." + today.getFullYear();
-		}
-		display.range = "School resumes: " + (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-		display.end = false;
-	}
-	else if(sdow.indexOf(date) != -1)
-	{
-		display.schedule = "SDOW";
-		var periodData = getPeriod([515,569,618,667,697,722,752,777,807,832,881], [564,613,662,687,717,742,772,797,827,876,925], ['1','2','3','4A','4B','5A','5B','6A','6B','7','8']);
-		display.period = periodData.period;
-		display.end = periodData.end;
-		display.range = periodData.range;
-		
-	}
-	else if(latearriv.indexOf(date) != -1)
-	{
-		display.schedule = "Late Arrival";
-		var periodData = getPeriod([630,670,705,740,780,820,860,895], [665,700,735,775,815,855,890,925], ['1','2','3','4','5','6','7','8']);
-		display.period = periodData.period;
-		display.end = periodData.end;
-		display.range = periodData.range;
-		
-	}
-	else if(actper.indexOf(date) != -1)
-	{
-		display.schedule = "Activity Period";
-		var periodData = getPeriod([485,535,580,630,675,705,730,760,785,815,840,885], [530,575,625,670,695,725,750,780,805,835,880,925], ['1','2','Activity','3','4A','4B','5A','5B','6A','6B','7','8']);
-		display.period = periodData.period;
-		display.end = periodData.end;
-		display.range = periodData.range;
-	}
-	else if(pmasm.indexOf(date) != -1)
-	{
-		display.schedule = "PM Assembly";
-		var periodData = getPeriod([485,535,580,625,675,725,775,825,865],[530,575,620,670,720,770,820,860,925],["1","2","3","4","5","6","7","8","Assembly"]);
-		display.period = periodData.period;
-		display.end = periodData.end;
-		display.range = periodData.range;
-	}
-	else if(finals.indexOf(date) != -1)
-	{
-		display.schedule = "Finals";
-		var periodData = getPeriod([485,585,685], [575, 675, 775],["1, 3, 8","2, 7, 6","4, 5, Makeup"])
-		display.period = periodData.period;
-		display.end = periodData.end;
-		display.range = periodData.range;
-	}
-	else if(summer.indexOf(date)!= -1){
-		display.schedule = "Summer School";
-		if(localStorage.break1 && localStorage.break2) {
-			var br1 = parseInt(localStorage.break1), br2 = parseInt(localStorage.break2);
-			var periodData = 
-getPeriod([465,br1,br1+15,br2,br2+25],[br1,br1+15,br2,br2+25,770],["School","Break 1","School","Break 2","School"]);
-			$("#clearBreaks").css("display","initial");
-		}
-		else{
-			var periodData = getPeriod([465],[770],[""]);
-			$("#clearBreaks").css("display","none");
-			setTimeout(function(){ $("#summer").css("display", "initial"); $("#summer").animate({top: "10px"},"slow"); },1000);
-		}
-		display.period = null
-		display.end = periodData.end;
-		display.range = periodData.range;
-	}
-	else
-	{
-		display.schedule = "Standard Schedule";
-		var periodData = getPeriod([485,545,600,655,685,710,740,765,795,820,875],[540,595,650,675,705,730,760,785,815,870,925],['1','2','3','4A','4B','5A','5B','6A','6B','7','8']);
-		display.period = periodData.period;
-		display.end = periodData.end;
-		display.range = periodData.range;
-	}
-	
-	$("#range").text(display.range);
-	$("#schedule").text(display.schedule);
-	$("#period").text(display.period);
- 	
- 	var secondsLeft = (display.end - minutes - 1) * 60 + (60 - today.getSeconds());
- 	var seconds;
- 	var countdown = function(){
- 	 	seconds = secondsLeft % 60;
- 		$("#timer").text(Math.floor(secondsLeft / 60) + ":" + (seconds < 10 ? "0" + seconds : seconds));
-		if(secondsLeft <= 0)
-		{
-			clearInterval(timer);
-			$("#timer").text("");
-			main();		
-		}
- 		secondsLeft--;
- 	}
- 	if(display.end) 
- 	{
- 			timer = setInterval(countdown, 1000);
- 			countdown();
- 	}
+    //Weekend and Holidays, find the next starting date
+    if (!(today.getDay() % 6) || constants.holidays.indexOf(date) != -1)
+    {
+        display.schedule = "No school";
+        display.period = "";
+        //Find the next school day
+        while (!(today.getDay() % 6) || constants.holidays.indexOf(date) != -1)
+        {
+            today.setDate(today.getDate() + 1);
+            date = getDateString(today);
+        }
+        display.range = "School resumes: " + getDateString(today, "/");
+        display.end = false;
+    }
+    //Late Arrival
+    else if (constants.latearrival.indexOf(date) != -1)
+    {
+        display = getPeriod(constants.latearrival_s, constants.latearrival_e, constants.latearrival_p);
+        display.schedule = "Late Arrival";
+    }
+    //Activity Period
+    else if (constants.activityperiod.indexOf(date) != -1)
+    {
+        display = getPeriod(constants.activityperiod_s, constants.activityperiod_e, constants.activityperiod_p);
+        display.schedule = "Activity Period";
+    }
+    //PM Assembly
+    else if (constants.pmassembly.indexOf(date) != -1)
+    {
+        display = getPeriod(constants.pmassembly_s, constants.pmassembly_e, constants.pmassembly_p);
+        display.schedule = "PM Assembly";
+    }
+    //Standard Schedule
+    else
+    {
+        display = getPeriod(constants.standard_s, constants.standard_e, constants.standard_p);
+        display.schedule = "Standard Schedule";
+    }
+
+    //Update the UI
+    $("#schedule").text(display.schedule);
+    $("#period").text(display.period);
+    $("#range").text(display.range);
+
+    //Timer
+    var secondsLeft = (display.end - minutes - 1) * 60 + (60 - today.getSeconds());
+    var seconds;
+    var countdown = function()
+    {
+        seconds = secondsLeft % 60;
+        //set timer to minutes + seconds
+        $("#timer").text(Math.floor(secondsLeft / 60) + ":" + (seconds < 10 ? "0" + seconds : seconds));
+        //reset if time has expired
+        if (secondsLeft <= 0)
+        {
+            clearInterval(timer);
+            $("#timer").text("");
+            main();
+        }
+        secondsLeft--;
+    }
+    //Only start the timer if we have an "end" value
+    if (display.end)
+    {
+        timer = setInterval(countdown, 1000);
+        countdown();
+    }
 }
 
+//Returns the current date formatted as a string
+function getDateString(date, sep = ".")
+{
+	return (date.getMonth() + 1) + sep + date.getDate() + sep + date.getFullYear();
+}
+
+//Returns the current period
 function getPeriod(starts, ends, periods)
 {
-	if(minutes < starts[0] || minutes >= ends[ends.length - 1]) return {period: "None", end: false, range: ""}
-	var last;
-	for(var i = 0; i < starts.length; i++)
+	//If school hasn't begun or has already ended
+    if (minutes < starts[0] || minutes >= ends[ends.length - 1])
 	{
-		if(minutes >= starts[i])
-		{
-			last = i
-	    	if(minutes < ends[i])
-			{
-				return  { period: "Period " + periods[i], end: ends[i], range: toHours(starts[i]) + "-" + toHours(ends[i]) }
-			}
-		}
+		return {
+        	period: "None",
+        	end: false,
+        	range: ""
+    	}
 	}
+	//Else we can go on and find the period
+    var last;
+    for (var i = 0; i < starts.length; i++)
+    {
+        if (minutes >= starts[i]) //if we are into the i-th period
+        {
+            last = i //this is the current last period
+            if (minutes < ends[i]) //if we're also before the end of the i-th period,
+            					   //then this is the current period
+            {
+                return {
+                    period: "Period " + periods[i],
+                    end: ends[i],
+                    range: toHours(starts[i]) + "-" + toHours(ends[i])
+                }
+            }
+        }
+    }
 
-	return { period: "Passing", end: starts[last+1], range: "Next: " + toHours(starts[last+1]) + "-" + toHours(ends[last+1])}
+    //If we're in school but a current period is not found, we're in a passing period
+    return {
+        period: "Passing",
+        end: starts[last + 1],
+        range: "Next: " + toHours(starts[last + 1]) + "-" + toHours(ends[last + 1])
+    }
 }
 
+//Converts a minutes input into hours:minutes format
 function toHours(minutes)
 {
-	var hours = Math.floor(minutes / 60);
-	if(hours > 12) hours -= 12;
-	var minutes = minutes % 60;
-	if(minutes < 10) minutes = "0" + minutes;
-	return hours + ":" + minutes;
-}
-
-function setBreaks(){
-	localStorage.break1 = $("#br1 option:selected").val();
-	localStorage.break2 = $("#br2 option:selected").val();
-	$("#summer").animate({top: "-150px"},"slow");
-	setTimeout(function() { $("#summer").css("display","none"); }, 1500);
-	clearInterval(timer);
-	main();
-}
-
-function clearBreaks(){
-	localStorage.removeItem("break1");
-	localStorage.removeItem("break2");
-	clearInterval(timer);
-	main();
+    var hours = Math.floor(minutes / 60);
+    if (hours > 12) hours -= 12;
+    var minutes = minutes % 60;
+    if (minutes < 10) minutes = "0" + minutes;
+    return hours + ":" + minutes;
 }
--- js/colors.js
+++ js/colors.js
@@ -1,18 +1,14 @@
-$(function() {
+$(function()
+{	
+	shuffle(constants.colors);
 
-	var colors = "#1693A5 #02AAB0 #00CDAC #F04155 #FF823A #95CFB7 #F56991 #FF9F80 #4F2958 #5B7C8D #66B6AB #5896E6 #95DE52 #D34747 #2B7F7F #A23774".split(" ");
-	
-	shuffle(colors);
-
-	$("#colors").children().each(function(index) {
-
-		$(this).css("backgroundColor", colors[index])
-
+	$("#tiles").children().each(function(index)
+	{
+		$(this).css("backgroundColor", constants.colors[index])
 	});
-	
 });
 
-function shuffle(a, b, c, d) {
-	
+function shuffle(a, b, c, d)
+{	
 	c = a.length; while (c) b = Math.random() * c-- | 0, d = a[c], a[c] = a[b], a[b] = d
 }
