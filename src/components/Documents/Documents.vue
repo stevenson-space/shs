98e8bd3e82a14bfad973422c99bb13fe881b10bf
Chaldron <aditya.mansha@gmail.com>
2020-01-03 14:39:40 -0600

    add document card type

--- src/components/Documents/Documents.vue
+++ src/components/Documents/Documents.vue
@@ -1,24 +1,35 @@
 <template>
 	<div>
 		<plain-header title="Documents" />
-		books go here
-		<card-container class="container"></card-container>
+		<card-container>
+			<documents-card class-name="AP Chemistry" :documents="docs" />
+		</card-container>
+		<form name="form-name" method="post" data-netlify="true">
+			<input name="ask-question" value="ask-question" />
+
+			<button>Submit</button>
+		</form>
 	</div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import CardContainer from "common/CardContainer.vue";
+import DocumentsCard from "common/cards/DocumentsCard.vue";
 import PlainHeader from "common/PlainHeader.vue";
 
 export default {
 	components: {
 		CardContainer,
+		DocumentsCard,
 		PlainHeader
 	},
 	data() {
-		return {};
-	}
+		return {
+			docs: [{ title: "book", link: "http://google.com" }]
+		};
+	},
+	methods: {}
 };
 </script>
 
