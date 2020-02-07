fc7aee6d18407d8be38a8c51091522f48cbdac1d
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-02-06 23:49:21 -0600

    Add shsHacks card

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,125 +1,129 @@
 <template>
-	<div :class="{ 'no-overflow': fullScreenMode }">
-		<schedule-header
-			:full-screen-mode="fullScreenMode"
-			@toggle-fullscreen="fullScreenMode = !fullScreenMode"
-		/>
+  <div :class="{ 'no-overflow': fullScreenMode }">
+    <schedule-header
+      :full-screen-mode="fullScreenMode"
+      @toggle-fullscreen="fullScreenMode = !fullScreenMode"
+    />
 
-		<card-container>
-			<schedule-card />
+    <card-container>
+      <shs-hacks-card />
+      
+      <schedule-card />
 
-			<lunch-card />
+      <lunch-card />
 
-			<upcoming-events-card />
+      <upcoming-events-card />
 
-			<icon-text-card
-				:icon="icons.faBell"
-				text="Bell Schedules"
-				link="bellschedules"
-			/>
+      <icon-text-card
+        :icon="icons.faBell"
+        text="Bell Schedules"
+        link="bellschedules"
+      />
 
-			<icon-text-card
-				:icon="icons.faLink"
-				text="Links"
-				link="links"
-				:invert="true"
-			/>
+      <icon-text-card
+        :icon="icons.faLink"
+        text="Links"
+        link="links"
+        :invert="true"
+      />
 
-			<icon-text-card
-				:icon="icons.faCalendarAlt"
-				text="Calendar"
-				link="calendar"
-			/>
+      <icon-text-card
+        :icon="icons.faCalendarAlt"
+        text="Calendar"
+        link="calendar"
+      />
 
-			<icon-text-card
-				:icon="icons.faCalculator"
-				text="GPA Calculator"
-				link="old/gpacalc.html"
-				:link-props="{ type: 'a' }"
-				:invert="true"
-			/>
+      <icon-text-card
+        :icon="icons.faCalculator"
+        text="GPA Calculator"
+        link="old/gpacalc.html"
+        :link-props="{ type: 'a' }"
+        :invert="true"
+      />
 
-			<icon-text-card
-				:icon="icons.faTint"
-				text="Switch Color"
-				link="colors"
-			/>
+      <icon-text-card
+        :icon="icons.faTint"
+        text="Switch Color"
+        link="colors"
+      />
 
-			<icon-text-card
-				:icon="icons.faTools"
-				text="Tools"
-				link="tools"
-				:invert="true"
-			/>
+      <icon-text-card
+        :icon="icons.faTools"
+        text="Tools"
+        link="tools"
+        :invert="true"
+      />
 
-			<icon-text-card
-				:icon="icons.faFileAlt"
-				text="Documents"
-				link="documents"
-			/>
+      <icon-text-card
+        :icon="icons.faFileAlt"
+        text="Documents"
+        link="documents"
+      />
 
-			<icon-text-card
-				:icon="icons.faCog"
-				text="Settings"
-				link="settings"
-				:invert="true"
-			/>
-		</card-container>
-	</div>
+      <icon-text-card
+        :icon="icons.faCog"
+        text="Settings"
+        link="settings"
+        :invert="true"
+      />
+    </card-container>
+  </div>
 </template>
 
 <script>
 import {
-	faBell,
-	faLink,
-	faFileAlt,
-	faCalendarAlt,
-	faTv,
-	faTint,
-	faCalculator,
-	faCog,
-	faTools
-} from "@fortawesome/free-solid-svg-icons";
+  faBell,
+  faLink,
+  faFileAlt,
+  faCalendarAlt,
+  faTv,
+  faTint,
+  faCalculator,
+  faCog,
+  faTools,
+} from '@fortawesome/free-solid-svg-icons';
 
-import CardContainer from "common/CardContainer.vue";
-import UpcomingEventsCard from "common/cards/UpcomingEventsCard.vue";
-import LunchCard from "common/cards/LunchCard.vue";
-import IconTextCard from "common/cards/IconTextCard.vue";
-import ScheduleCard from "common/cards/ScheduleCard.vue";
-import ScheduleHeader from "./Header.vue";
+import CardContainer from 'common/CardContainer.vue';
+import UpcomingEventsCard from 'common/cards/UpcomingEventsCard.vue';
+import LunchCard from 'common/cards/LunchCard.vue';
+import IconTextCard from 'common/cards/IconTextCard.vue';
+import ScheduleCard from 'common/cards/ScheduleCard.vue';
+import ShsHacksCard from 'common/cards/ShsHacksCard.vue';
+import ScheduleHeader from './Header.vue';
 
 export default {
-	components: {
-		ScheduleHeader,
-		CardContainer,
-		UpcomingEventsCard,
-		LunchCard,
-		IconTextCard,
-		ScheduleCard
-	},
-	data() {
-		return {
-			icons: {
-				faBell,
-				faLink,
-				faFileAlt,
-				faCalendarAlt,
-				faTv,
-				faTint,
-				faCalculator,
-				faCog,
-				faTools
-			},
-			fullScreenMode: false
-		};
-	},
-	created() {
-		// Sometimes the interval used in Header.vue stops when the tab leaves focus
-		// so updating the date when focus returns is necessary
-		window.addEventListener("focus", () => {
-			this.$store.commit("setCurrentTime");
-		});
-	}
+  components: {
+    ScheduleHeader,
+    CardContainer,
+    UpcomingEventsCard,
+    LunchCard,
+    IconTextCard,
+    ScheduleCard,
+    ShsHacksCard,
+  },
+  data() {
+    return {
+      icons: {
+        faBell,
+        faLink,
+        faFileAlt,
+        faCalendarAlt,
+        faTv,
+        faTint,
+        faCalculator,
+        faCog,
+        faTools,
+      },
+      fullScreenMode: false,
+    };
+  },
+  created() {
+    // Sometimes the interval used in Header.vue stops when the tab leaves focus
+    // so updating the date when focus returns is necessary
+    window.addEventListener('focus', () => {
+      this.$store.commit('setCurrentTime');
+    });
+  },
 };
 </script>
 
--- src/components/common/cards/ShsHacksCard.vue
+++ src/components/common/cards/ShsHacksCard.vue
@@ -2,19 +2,11 @@
   <card>
     <a class="main-link" href="https://shshacks.io" target="_blank">
       <img class="logo" src="static/shsHacks.png">
-      <div class="text">
-        <div class="name">shsHacks</div>
-        <div class="register">Deadline: 8:59pm</div>
+      <div class="register">
+        Register by <span class="color">11:59pm</span> on Monday! &nbsp;
+        <font-awesome-icon :icon="faExternalLinkAlt" class="link-icon" />
       </div>
-      <font-awesome-icon :icon="faExternalLinkAlt" class="link-icon" />
     </a>
-
-    <div class="line" />
-
-    <div class="deregister">
-      Please click <a class="emphasize" href="https://goo.gl/forms/K0f8b5yYHJqYiRHz1" target="_blank">here</a>
-      &nbsp;if you registered but can <span class="emphasize">NOT</span> attend
-    </div>
   </card>
 </template>
 
@@ -39,40 +31,24 @@ export default {
   align-items: center
   text-decoration: none
   color: black
+  justify-content: center
+  flex-direction: column
 
   .logo
-    height: 55px
-    margin: 5px 0
+    height: 85px
+    padding: 10px 5px
 
-  .text
-    flex: 1
-
-    .name
-      color: #275D38
+  .register
+    font-size: .85em
+    text-align: center
+    padding-bottom: 10px
+    .color
+      color: var(--color)
       font-weight: bold
-      font-size: 1.4em
 
-    .register
-      font-size: .75em
+    .link-icon
+      font-size: 1.1em
+      color: var(--color)
 
-  .link-icon
-    margin-right: 15px
-    font-size: 1.25em
-    color: var(--color)
-
-.line
-  height: 2px
-  background-color: var(--color)
-  width: 100%
-
-.deregister
-  font-size: .7em
-  text-align: center
-  margin: 7px 0
-
-  .emphasize
-    color: var(--color)
-    font-weight: bold
-    font-size: 1.25em
 
 </style>
