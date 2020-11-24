fd8d870f67fb5cf30a58a2a5e625f390b868f86f
Joseph <31457361+jshepin@users.noreply.github.com>
2020-11-24 12:58:17 -0600

    Thanksgiving Update
    This update includes the updated early release schedule for 11/25/2020 and a "happy thanksgiving" card

---
+++ src/components/common/cards/HolidayCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <card>
+    <img class="logo" src="static/thanksgiving.jpg" />
+    <div class="message">Have a happy Thanksgiving break! &nbsp;</div>
+  </card>
+</template>
+
+<script>
+import Card from "common/Card.vue";
+
+export default {
+  components: { Card, },
+ 
+};
+</script>
+
+<style lang="sass" scoped>
+.logo
+    display: block
+    margin-left: auto
+    margin-right: auto
+    height: 85px
+    padding: 10px 5px
+
+.message
+    font-size: .85em
+    text-align: center
+    padding-bottom: 10px
+    .color
+
+</style>
