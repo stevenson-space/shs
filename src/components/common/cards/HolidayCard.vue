f4e69dc900ed420e50064f50140e4fb4d4f2a3fe
Joseph <31457361+jshepin@users.noreply.github.com>
2020-12-04 12:17:35 -0600

    Finals Schedule  + Holiday Card
    -  finals schedule (I changed the name so the caching issue fiasco that happened on Thanksgiving does not happen again)
     - winter holiday card (this time the card only shows up a few days before finals and ends on January 6th)

--- src/components/common/cards/HolidayCard.vue
+++ src/components/common/cards/HolidayCard.vue
@@ -1,31 +1,37 @@
 <template>
-  <card>
-    <img class="logo" src="static/thanksgiving.jpg" />
-    <div class="message">Have a happy Thanksgiving break! &nbsp;</div>
+  <card v-show="showCard">
+    <img class="logo" src="static/snowman.png" />
+    <div class="message">Have a great winter break!</div>
   </card>
 </template>
-
 <script>
 import Card from "common/Card.vue";
 
 export default {
-  components: { Card, },
- 
+  components: { Card },
+  computed: {
+    showCard() {
+      //start December 14, 2020, end January 6, 2021 
+      var startTime = 1607925600; //in epoch miliseconds 
+      var endTime = 1609912800;
+      var today = new Date() / 1000;
+      return today > startTime && today < endTime;
+    },
+  },
 };
 </script>
 
 <style lang="sass" scoped>
 .logo
-    display: block
-    margin-left: auto
-    margin-right: auto
-    height: 85px
-    padding: 10px 5px
+  display: block
+  margin-left: auto
+  margin-right: auto
+  height: 85px
+  padding: 10px 5px
 
 .message
-    font-size: .85em
-    text-align: center
-    padding-bottom: 10px
-    .color
-
+  font-size: .85em
+  text-align: center
+  padding-bottom: 10px
+  .color
 </style>
