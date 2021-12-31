113b7eaa2826d475f70a709d4f2021bd11dd3466
Joseph Shepin <jshepin@gmail.com>
2021-12-30 23:26:15 -0600

    added comments, color conflict test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -19,22 +19,27 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('SHS Tests', function() {
     it('Home', function() {
+        //test moving between days
         cy.visit('localhost:8080/?date=1-1-2022')
         cy.contains('Saturday, January 1')
         cy.get('.main > :nth-child(3)').click()
         cy.contains('Sunday, January 2')
 
+        //test that the main buttons are on the screen
         cy.visit('localhost:8080')
         cy.contains('Links')
         cy.contains('Schedule')
         cy.contains('Settings')
         cy.contains("Color")
+
+        //test full screen mode
         cy.get('.full-screen-mode').click()  //tests if the color toggle works
         cy.get('.header').should('have.css', 'background-color', 'rgb(27, 94, 32)')
         cy.get('.remove-color').click()
         cy.get('.header').should('have.css', 'background-color', 'rgb(255, 255, 255)')
     })
     it('/BellSchedules', function() {
+        //test switching bell schedules
         cy.visit('localhost:8080/?date=1-1-2022')
         cy.contains('Bell Schedules').click()
         cy.contains('Standard')
@@ -42,16 +47,19 @@ describe('SHS Tests', function() {
         cy.get(':nth-child(1) > .wrapper > .schedule-select > .dropdown > .select-option').click()
         cy.contains("Half Periods").click()
         cy.contains("1B")
-        cy.get('.home').click()
+        
+        cy.get('.home').click() //return back home
         cy.contains("Links")
     })
     it('/Links', function() {
+        //Make sure links appear
         cy.visit('localhost:8080/?date=1-1-2022')
         cy.contains('Links').click()
         cy.contains('D125')
         cy.contains('Testing Center')
     })
     it('/Tools', function() {
+        //Test Adding Time to Timer
         cy.visit('localhost:8080')
         cy.contains('Tools').click() //click on tools page
         cy.get('.add-time-buttons > :nth-child(1)').click() //add time to the timer
@@ -63,17 +71,29 @@ describe('SHS Tests', function() {
         cy.get('.home').click() //go back home
         cy.contains("Links")
     })
-    
+
     it('/Colors', function() {
+        //Test Changing Themes
         cy.visit('localhost:8080/colors')
         cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 255)')
-        cy.contains("Dark").click() //change theme
+        cy.contains("Dark").click() //change to dark theme
         cy.get('body').should('have.css', 'background-color', 'rgb(35, 39, 42)')
         cy.get('.custom-color').contains("#b38825")
 
-        cy.get('.color').first().click() //set a custom color
+        //Test Setting a Custom Color
+        cy.get('.color').first().click() 
         cy.get('.shade').first().click()
         cy.get('.custom-color').contains("#ffcdd2")
         cy.get('.custom-color').should('have.css', 'color', 'rgb(255, 205, 210)')
+        
+        //Test Color Conflict
+        cy.contains("Light").click() //change back to light theme
+        cy.contains("Color Conflict")
+        cy.contains("No").click()
+        cy.get('.custom-color').contains("#ffcdd2")
+        cy.contains("Dark").click()
+        cy.contains("Color Conflict")
+        cy.contains("Yes").click()
+        cy.get('.custom-color').contains("#b38825")
     })
 })
