c19cd2e4330ff9a0a56d1e73ac3c770932bf3e5b
Joseph Shepin <jshepin@gmail.com>
2021-12-30 22:46:19 -0600

    more sophisticated test for home page

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -17,16 +17,25 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
 
-describe('Stevenson.Space Tests', function() {
+describe('SHS Tests', function() {
     it('Home', function() {
+        cy.visit('localhost:8080/?date=1-1-2022')
+        cy.contains('Saturday, January 1')
+        cy.get('.main > :nth-child(3)').click()
+        cy.contains('Sunday, January 2')
+
         cy.visit('localhost:8080')
         cy.contains('Links')
         cy.contains('Schedule')
         cy.contains('Settings')
         cy.contains("Color")
+        cy.get('.full-screen-mode').click()  //tests if the color toggle works
+        cy.get('.header').should('have.css', 'background-color', 'rgb(27, 94, 32)')
+        cy.get('.remove-color').click()
+        cy.get('.header').should('have.css', 'background-color', 'rgb(255, 255, 255)')
     })
     it('/BellSchedules', function() {
-        cy.visit('localhost:8080')
+        cy.visit('localhost:8080/?date=1-1-2022')
         cy.contains('Bell Schedules').click()
         cy.contains('Standard')
         cy.contains("1B").should('not.exist')
@@ -37,7 +46,7 @@ describe('Stevenson.Space Tests', function() {
         cy.contains("Links")
     })
     it('/Links', function() {
-        cy.visit('localhost:8080')
+        cy.visit('localhost:8080/?date=1-1-2022')
         cy.contains('Links').click()
         cy.contains('D125')
         cy.contains('Testing Center')
