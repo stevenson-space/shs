63d3354c0f0fbbbddbd22e924ba6372d1a5a8db0
Joseph Shepin <jshepin@gmail.com>
2022-03-19 09:47:21 -0500

    add QR styling

--- src/views/QRCodes/QRCodes.vue
+++ src/views/QRCodes/QRCodes.vue
@@ -3,12 +3,13 @@
     <plain-header title='QR Codes' />
     <card class="qr-card">
       <div class="center" >
-      <div id='qr-code' ref='qrCode'></div>
+      <div id='qr-code' :class="{ 'show' : showQR }" ref='qrCode'></div>
       <br>
       <input v-model='enteredQRCode' placeholder='Enter A Valid Link' />
       <div class="input-tip"  v-if="enteredQRCode.length > 40">Tip: For very long links, consider using <a href="https://bitly.com/" target='_blank'>Bitly</a> for a more aesthetic code</div>
+      <div class="input-tip"  v-if="errorMessage.length > 0">{{ errorMessage }}</div>
       <div class="btn-row">
-        <rounded-button v-if="isValidLink" class="button" @click="generateQR" :text="showQR && options.data != enteredQRCode ? 'Re-Generate' : 'Generate'" :circular="false"/>
+        <rounded-button v-if="isValidLink()" class="button" @click="generateQR" :text="showQR && options.data != enteredQRCode ? 'Re-Generate' : 'Generate'" :circular="false"/>
         <rounded-button v-if="showQR" class="button"  @click='download' text='Download' :circular="false"/>
       </div>
       </div>
@@ -30,8 +31,8 @@ export default {
   },
   data: () => {
     const options = {
-      width: 400,
-      height: 400,
+      width: 500,
+      height: 500,
       type: 'png',
       data: '',
       image: 'static/QRCodeLogo.svg',
@@ -64,21 +65,29 @@ export default {
       showQR: false,
       options,
       enteredQRCode: '',
+      errorMessage: '',
       qrCode: new QRCodeStyling(options),
     };
   },
   mounted() {
     this.qrCode.append(this.$refs.qrCode);
   },
-  computed: {
+  methods: {
     isValidLink() {
       const { enteredQRCode } = this;
       const linkExpression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
       const linkValidator = new RegExp(linkExpression);
-      return this.options.data !== enteredQRCode && enteredQRCode.length > 0 && enteredQRCode.length < 120 && enteredQRCode.match(linkValidator);
+      const isNonEmptyURL = this.enteredQRCode.length > 0;
+      if (!enteredQRCode.match(linkValidator)) {
+        this.errorMessage = isNonEmptyURL ? 'Invalid Link Format' : '';
+        return false;
+      } if (enteredQRCode.length > 120) {
+        this.errorMessage = isNonEmptyURL ? 'Link too long' : '';
+        return false;
+      }
+      this.errorMessage = '';
+      return this.options.data !== enteredQRCode && isNonEmptyURL;
     },
-  },
-  methods: {
     generateQR() {
       if (this.isValidLink) {
         console.log('generating new qr code');
@@ -97,6 +106,11 @@ export default {
 
 <style lang="sass" scoped>
 @import 'src/styles/style.sass'
+#qr-code
+  &.show
+    transform: scale(.5)
+    margin-top: -110px
+    margin-bottom: -110px
 .qr-card
   max-width: 972px
   padding: 20px
