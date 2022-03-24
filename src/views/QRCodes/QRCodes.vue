0798fac4cf528ba6631af15358fdccece5f14313
Joseph Shepin <jshepin@gmail.com>
2022-03-24 17:33:45 -0600

    remove default qr value

--- src/views/QRCodes/QRCodes.vue
+++ src/views/QRCodes/QRCodes.vue
@@ -64,7 +64,7 @@ export default {
     return {
       showQR: false,
       options,
-      enteredQRCode: 'https://github.com',
+      enteredQRCode: '',
       errorMessage: '',
       qrCode: new QRCodeStyling(options),
     };
