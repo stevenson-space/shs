b46fdb8e62bf1bbe5e9257f44b79385ef0bcf30d
Joseph Shepin <jshepin@gmail.com>
2022-04-17 01:38:05 -0500

    fix google auth

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,10 @@
   // This fixes a webpack thing for the google authentication library.
   module.exports = {
     configureWebpack: (config) => {
-        config.resolve.fallback = {
-        ...config.resolve.fallback,
-        url: false,
-        util: false,
-        querystring: false,
-        https: false,
-        };
+      config.resolve.fallback = {
+      ...config.resolve.fallback,
+      querystring: require.resolve("querystring-es3"),
+      };
     }
   }
+
