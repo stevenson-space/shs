e48c3fd96e29ce1685cf7ce1c9a6f9ccc3de179c
Joseph Shepin <jshepin@gmail.com>
2022-05-18 22:28:41 -0500

    Girlcon card

--- src/components/cards/NewFeatureCard.vue
+++ src/components/cards/NewFeatureCard.vue
@@ -1,40 +1,44 @@
 <template>
-  <timed-card startTime="March 26, 2022" endTime="May 3, 2022">
-     <div class="icon">
-      <font-awesome-icon :icon="icons.faQrcode" />
-     </div>
-    <div class="message"><b>Introducing Stevenson.Space QR Codes 🎉</b><p>Styled with Stevenson colors and logo</p><a href="/qr">Try It Out</a></div>
+  <timed-card startTime="May 18, 2022" endTime="May 24, 2022">
+    <div class="container">
+      <img class="logo" src="static/occasions/GirlconLogo.png">
+      <div class="message">
+        Girlcon is a free conference aimed at empowering women in stem and it's happening this summer held @Expedia Chicago and virtually!
+        <a href="https://GirlCon.org" class="link" target="_blank">Learn more</a>
+      </div>
+    </div>
   </timed-card>
 </template>
 
 <script>
-import { faQrcode } from '@fortawesome/free-solid-svg-icons';
-import TimedCard from './TimedCard.vue';
+import TimedCard from '@/components/cards/TimedCard.vue';
 
 export default {
   components: { TimedCard },
-  data() {
-    return {
-      icons: {
-        faQrcode,
-      },
-    };
-  },
 };
 </script>
 
 <style lang="sass" scoped>
-.icon
-  font-size: 40px
+.container
   display: flex
+  align-items: center
+  text-decoration: none
   justify-content: center
-  padding: 10px
-  color: var(--color)
-.message
-  text-align: center
-  font-size: .9em
-  padding-bottom: 10px
-  line-height: 6px
-  a
+  flex-direction: column
+  color: var(--primary)
+  .link
     color: var(--color)
+  .logo
+    height: 165px
+    margin: 12px 10px
+    background: white
+    border-radius: 100%
+  .message
+    font-size: .85em
+    text-align: left
+    padding-bottom: 10px
+    margin: 0 20px
+    line-height: 1.6em
+    text-align: center
+
 </style>
