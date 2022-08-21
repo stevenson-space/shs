b2cb75e7a056330e8be755f6a281b246253637de
andrew <me@andreww.co>
2022-08-20 22:58:45 -0500

    add form in Contact

--- src/views/Settings/Contact.vue
+++ src/views/Settings/Contact.vue
@@ -1,8 +1,8 @@
 <template>
   <settings-section title="Contact">
     <br />
-    Feel free to address any questions, comments, or concerns to
-    <a href="mailto:admin@stevenson.space">admin@stevenson.space</a>. You may also write a issue if it's code-related <b><a href="https://github.com/stevenson-space/shs/issues" target='_blank'>here</a></b>.
+    Feel free to address any questions, comments, or concerns by
+    <a href="/getHelp">visiting our contact form</a>. You may also write a issue if it's code-related <b><a href="https://github.com/stevenson-space/shs/issues" target='_blank'>here</a></b>.
   </settings-section>
 </template>
 
@@ -20,5 +20,6 @@ export default {
 <style lang="sass" scoped>
 a
     text-decoration: none
+    font-weight: bold
     color: var(--color)
 </style>
