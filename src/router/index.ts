3e5a41726fff444f4d959191b7ad555a6ed7e1d0
andrew <me@andreww.co>
2023-01-06 21:04:48 -0600

    Revert "Merge branch 'music' into main"
    This reverts commit 9b5ce4cc23ddbce5b9f4b8aa744fcaf28d0701df, reversing
    changes made to 2449e1f4189ab245d6c045337e64ee1732146b2e.

--- src/router/index.ts
+++ src/router/index.ts
@@ -96,11 +96,6 @@ const routes: Array<RouteRecordRaw> = [
     path: '/getHelp',
     component: GetHelp,
   },
-  {
-    name: 'Jukebox',
-    path: '/jukebox',
-    component: Jukebox,
-  },
 ];
 
 const router = createRouter({
