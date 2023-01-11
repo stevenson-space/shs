b975879f833bac11ffde4ed3d5ec06ba6efcc7dc
andrew <me@andreww.co>
2023-01-10 21:32:20 -0600

    add new patriot dollars system

--- src/views/Links/Links.vue
+++ src/views/Links/Links.vue
@@ -47,7 +47,7 @@ export default {
         { name: 'Naviance', url: 'https://student.naviance.com/aeshs', image: Naviance },
         { name: 'Peer Tutors', desc: 'Must be logged in using school google account', url: 'https://sites.google.com/d125.org/peer-tutors/home', image: PeerTutors },
         { name: 'Testing Center', url: 'https://docs.google.com/forms/d/e/1FAIpQLSdMoby5m1oNCMi_VgINPE-FDqPqjH5XlvgdDXUFxS4lJbb4Ng/viewform', image: TestingCenter },
-        { name: 'Patriot Dollars', desc: 'Patience Required', url: 'https://d125-sp.transactcampus.com/eaccounts/%28S%28hpb5gh0sgji4hrhmx2ymnvot%29%29/AnonymousHome.aspx', image: PatriotDollars },
+        { name: 'Patriot Dollars', desc: 'Patience Required', url: 'https://get.cbord.com/d125/full/login.php', image: PatriotDollars },
       ],
     };
   },
