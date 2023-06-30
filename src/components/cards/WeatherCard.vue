21ab61d281b7355e00b6f0293fb7e7753fe1c10e
Aaaaa501 <71293864+Aaaaa501@users.noreply.github.com>
2023-06-29 20:07:02 -0500

    Update WeatherCard.vue

--- src/components/cards/WeatherCard.vue
+++ src/components/cards/WeatherCard.vue
@@ -6,10 +6,17 @@
         <div class="info-container">
           <p class="info date">{{ weather.day }}</p>
           <div class="info condition">
-            <font-awesome-icon
-              class="condition-icon"
-              :icon="icons.faSun"
-            />
+            <Cloudy class="condition-icon" v-if="weather.cloudcover > 70 && weather.rain_percent < 40" />
+            <Rainy class="condition-icon" v-else-if="weather.cloudcover > 70 && weather.rain_percent >= 40" />
+            <div class="condition-icon" v-else-if="weather.cloudcover > 30 && weather.rain_percent < 40" >
+              <Sunny class="partly-sun" />
+              <Cloudy class="partly-cloudy" />
+            </div>
+            <div class="condition-icon" v-else-if="weather.cloudcover > 30 && weather.rain_percent >= 40" >
+              <Sunny class="partly-sun" />
+              <Rainy class="partly-rainy" />
+            </div>
+            <Sunny class="condition-icon" v-else/>
           </div>
           <div class="info temp">
             <p class="temp-high">{{ weather.temp_high }}°</p>
@@ -21,16 +28,20 @@
     <a class="attribution-link" href="https://open-meteo.com/">
       <div class="attribution">Weather data by Open-Meteo.com</div>
     </a>
+    <CloudIcon />
   </card>
 </template>
 
 <script>
 import { faTint, faCloud, faSun } from '@fortawesome/free-solid-svg-icons';
 import { mapState } from 'pinia';
+import Cloudy from 'vue-ionicons/dist/ios-cloudy.vue';
+import Rainy from 'vue-ionicons/dist/ios-rainy.vue';
+import Sunny from 'vue-ionicons/dist/ios-sunny.vue';
 import Card from '@/components/Card.vue';
 
 export default {
-  components: { Card },
+  components: { Card, Cloudy, Rainy, Sunny },
   props: {
     title: { type: String, default: 'Weather' },
   },
@@ -78,20 +89,22 @@ export default {
         return `${date.getFullYear()}-${month}-${day}`;
       }
       const startDate = formatDate(currentDate);
-      const endDate = formatDate(new Date(currentDate.setDate(currentDate.getDate() + 7)));
+      const endDate = formatDate(new Date(currentDate.setDate(currentDate.getDate() + 5)));
       currentDate = new Date();
-      fetch(`https://api.open-meteo.com/v1/forecast?latitude=42.26&longitude=-87.84&daily=weathercode,temperature_2m_max,temperature_2m_min&start_date=${startDate}&end_date=${endDate}&timezone=auto`)
+      fetch(`https://api.open-meteo.com/v1/forecast?latitude=42.26&longitude=-87.84&hourly=cloudcover&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&start_date=${startDate}&end_date=${endDate}&timezone=auto`)
         .then((response) => response.json())
         .then((data) => {
           const dailyData = [];
-          for (let i = 0; i < 7; i++) {
+          for (let i = 0; i < 5; i++) {
+            const avgCloudCover = data.hourly.cloudcover.slice(i * 24, (i + 1) * 24).reduce((a, c) => a + c, 0) / 24;
             const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
             const dayOfWeek = days[(currentDate.getDay() + i) % 7];
             const daily = {
               day: dayOfWeek,
               temp_high: Math.round(data.daily.temperature_2m_max[i]),
               temp_low: Math.round(data.daily.temperature_2m_min[i]),
-              weather_code: data.daily.weathercode[i],
+              rain_percent: data.daily.precipitation_probability_max[i],
+              cloudcover: avgCloudCover,
             };
             dailyData.push(daily);
           }
@@ -155,9 +168,21 @@ export default {
         place-items: center
 
         .condition-icon
-          font-size: 1.4em
+          font-size: 2em
           color: var(--color)
           margin: 0.5px 0
+          position: absolute
+          top:-15px
+
+          .partly-cloudy,
+          .partly-rainy
+            font-size: 1em
+
+          .partly-sun
+            font-size: 0.8em
+            position: absolute
+            left: 35%
+            bottom: 45%
 
   .attribution
     font-size: 0.5em
