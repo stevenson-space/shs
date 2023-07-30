4a2c2d5c3fb5662acf6196553d46f82521fc1b88
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2023-07-29 19:17:51 -0500

    countdown bug fixes

--- src/views/Pwc/Pwc.vue
+++ src/views/Pwc/Pwc.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="status-container">
-        <div class="countdown">{{ countdownString }}</div>
-        <div class="status" :class="{ open: isOpeningHours, closed: !isOpeningHours }">
-            {{ getStatus }}
-            <div class="day-date">{{ dayDate }}</div>
+    <div class="app">
+        <h1>PWC Gym Countdown</h1>
+        <div class="countdown">
+            <p v-if="isOpen">Time remaining until closure: {{ countdownTime }}</p>
+            <p v-else>Time remaining until next opening: {{ countdownTime }}</p>
         </div>
         <button @click="goToHomepage" class="home-button">Go Back to Homepage</button>
     </div>
@@ -12,200 +12,120 @@
 <script>export default {
         data() {
             return {
-                isOpen: false,
-                currentTime: new Date().getTime(),
+                openingTimes: [
+                    { day: 1, start: "15:30", end: "20:00" }, // Monday
+                    { day: 2, start: "15:30", end: "20:00" }, // Tuesday
+                    { day: 3, start: "15:30", end: "20:00" }, // Wednesday
+                    { day: 4, start: "15:30", end: "20:00" }, // Thursday
+                    { day: 5, start: "15:30", end: "18:00" }, // Friday
+                    { day: 6, start: "10:00", end: "15:00" }, // Saturday
+                    { day: 0, start: "10:00", end: "15:00" }, // Sunday
+                ],
+                currentTimeMs: 0,
             };
         },
         computed: {
+            currentTime() {
+                return new Date(this.currentTimeMs);
+            },
             currentDay() {
-                return new Date().getDay(); // 0: Sunday, 1: Monday, ..., 6: Saturday
+                return this.currentTime.getDay();
             },
-            currentHour() {
-                return new Date().getHours();
+            currentOpeningTime() {
+                const openingTime = this.openingTimes.find(
+                    (time) => time.day === this.currentDay
+                );
+                return this.getTimeInMs(this.currentTime.toDateString(), openingTime.start);
             },
-            currentMinute() {
-                return new Date().getMinutes();
-            },
-            isBeforeJune1() {
-                const now = new Date();
-                return now.getMonth() < 5 || (now.getMonth() === 5 && now.getDate() < 1);
-            },
-            isAfterAugust12() {
-                const now = new Date();
-                return now.getMonth() > 7 || (now.getMonth() === 7 && now.getDate() >= 12);
-            },
-            isPwcClosed() {
-                const pwcClosureStart = new Date('2023-08-09');
-                const pwcClosureEnd = new Date('2023-08-12');
-                const now = new Date();
-                return now >= pwcClosureStart && now <= pwcClosureEnd;
-            },
-            getStatus() {
-                const isWeekend = this.currentDay === 0 || this.currentDay === 6;
-
-                if (this.isPwcClosed) {
-                    return 'Closed';
-                } else if (this.isBeforeJune1 || this.isAfterAugust12) {
-                    const isOpeningHours =
-                        (this.currentDay >= 1 &&
-                            this.currentDay <= 4 &&
-                            this.currentHour >= 15 &&
-                            (this.currentHour < 20 || (this.currentHour === 20 && this.currentMinute === 0))) ||
-                        (this.currentDay === 5 &&
-                            this.currentHour >= 15 &&
-                            (this.currentHour < 18 || (this.currentHour === 18 && this.currentMinute === 0))) ||
-                        (isWeekend &&
-                            this.currentHour >= 10 &&
-                            (this.currentHour < 15 || (this.currentHour === 15 && this.currentMinute === 0)));
-
-                    return isOpeningHours ? 'Open' : 'Closed';
-                } else {
-                    const isOpeningHours =
-                        (this.currentDay >= 1 &&
-                            this.currentDay <= 4 &&
-                            this.currentHour >= 9 &&
-                            (this.currentHour < 17 || (this.currentHour === 17 && this.currentMinute === 0))) ||
-                        (this.currentDay === 5 &&
-                            this.currentHour >= 9 &&
-                            (this.currentHour < 13 || (this.currentHour === 13 && this.currentMinute === 0))) ||
-                        (this.currentDay === 6 &&
-                            this.currentHour >= 10 &&
-                            (this.currentHour < 14 || (this.currentHour === 14 && this.currentMinute === 0)));
-
-                    return isOpeningHours ? 'Open' : 'Closed';
-                }
+            currentClosingTime() {
+                const closingTime = this.openingTimes.find(
+                    (time) => time.day === this.currentDay
+                );
+                return this.getTimeInMs(this.currentTime.toDateString(), closingTime.end);
             },
             nextOpeningTime() {
-                const nextOpeningHour =
-                    this.currentDay === 5
-                        ? 9 // Friday: 9 a.m.
-                        : this.currentDay === 6
-                            ? 10 // Saturday: 10 a.m.
-                            : 9; // Monday to Thursday: 9 a.m.
-
-                if (this.isBeforeJune1 || this.isAfterAugust12) {
-                    return new Date(new Date().setHours(nextOpeningHour + 3, 30, 0, 0));
-                } else {
-                    return new Date(new Date().setHours(nextOpeningHour, 0, 0, 0));
+                const nextDay = (this.currentDay + 1) % 7;
+                const openingTime = this.openingTimes.find(
+                    (time) => time.day === nextDay
+                );
+                return this.getTimeInMs(this.getNextDate(nextDay), openingTime.start);
+            },
+            isOpen() {
+                return this.currentTime >= this.currentOpeningTime && this.currentTime <= this.currentClosingTime;
+            },
+            countdownTime() {
+                const targetTime = this.isOpen ? this.currentClosingTime : this.nextOpeningTime;
+                const timeDiff = targetTime - this.currentTimeMs;
+                if (timeDiff <= 0) {
+                    return "00:00:00";
                 }
-            },
-            nextClosingTime() {
-                const nextClosingHour =
-                    this.currentDay === 5
-                        ? 13 // Friday: 1 p.m.
-                        : this.currentDay === 6
-                            ? 14 // Saturday: 2 p.m.
-                            : 17; // Monday to Thursday: 5 p.m.
-
-                if (this.isBeforeJune1 || this.isAfterAugust12) {
-                    return new Date(new Date().setHours(nextClosingHour + 3, 30, 0, 0));
-                } else {
-                    return new Date(new Date().setHours(nextClosingHour, 0, 0, 0));
-                }
-            },
-            isOpeningHours() {
-                return this.getStatus === 'Open' || this.getStatus === 'Open (Saturday)';
-            },
-            dayDate() {
-                const options = { weekday: 'long', month: 'long', day: 'numeric' };
-                return new Date().toLocaleDateString('en-US', options);
-            },
-            countdownString() {
-                let countdown;
-                let isOpeningHours = this.isOpeningHours;
-                if (isOpeningHours) {
-                    countdown = this.nextClosingTime - this.currentTime;
-                } else {
-                    countdown = this.nextOpeningTime - this.currentTime;
-                    isOpeningHours = countdown > 0; // If countdown is greater than zero
-                }
-
-                if (countdown <= 0) return '00:00:00';
-
-                const seconds = Math.floor((countdown / 1000) % 60);
-                const minutes = Math.floor((countdown / (1000 * 60)) % 60);
-                const hours = Math.floor((countdown / (1000 * 60 * 60)) % 24);
-
-                return `${hours.toString().padStart(2, '0')}:${minutes
-                    .toString()
-                    .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-            },
-        },
-        watch: {
-            currentTime() {
-                this.$forceUpdate();
+                const hours = String(Math.floor(timeDiff / (1000 * 60 * 60))).padStart(2, '0');
+                const minutes = String(Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
+                const seconds = String(Math.floor((timeDiff % (1000 * 60)) / 1000)).padStart(2, '0');
+                return `${hours}:${minutes}:${seconds}`;
             },
         },
         methods: {
-            initializeCountdown() {
-                this.stopCountdown();
-                this.interval = setInterval(() => {
-                    this.currentTime = new Date().getTime();
-                }, 1000);
+            getTimeInMs(dateString, timeString) {
+                const dateTimeString = `${dateString} ${timeString}`;
+                return new Date(dateTimeString).getTime();
             },
-            stopCountdown() {
-                clearInterval(this.interval);
+            getNextDate(day) {
+                const today = this.currentTime.getDay();
+                const difference = (day - today + 7) % 7;
+                const nextDate = new Date(this.currentTime);
+                nextDate.setDate(this.currentTime.getDate() + difference);
+                return nextDate.toDateString();
+            },
+            updateCurrentTime() {
+                this.currentTimeMs = Date.now();
             },
             goToHomepage() {
                 // Use Vue Router to navigate to the homepage
                 this.$router.push("/");
-            }
+            },
         },
-        mounted() {
-            this.initializeCountdown();
-        },
-
         created() {
-            setInterval(() => {
-                this.$forceUpdate();
+            // Initialize the countdown immediately
+            this.updateCurrentTime();
+
+            // Update currentTime every second
+            this.timer = setInterval(() => {
+                this.updateCurrentTime();
             }, 1000);
+        },
+        beforeDestroy() {
+            clearInterval(this.timer);
         },
     };</script>
 
 <style>
-    .status-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
+    .app {
+        text-align: center;
+        margin-top: 50px;
+        background-color: #00563F;
+        color: #FFFFFF;
         height: 100vh;
-        background-color: #4caf50; /* Green background */
-        color: #ffffff; /* White text */
-    }
-
-    .logo {
-        width: 120px;
-        height: 120px;
-        margin-bottom: 20px;
+        display: flex;
+        flex-direction: column; /* Place the items in a column layout */
+        justify-content: center;
+        align-items: center;
     }
 
     .countdown {
-        font-size: 32px;
-        font-weight: bold;
-        color: #ffffff; /* White text */
-    }
-
-    .status {
-        margin-top: 20px;
         font-size: 24px;
         font-weight: bold;
-        padding: 10px;
-        border-radius: 8px;
-        text-align: center;
-        background-color: #4caf50; /* Green background for both "Open" and "Closed" status */
+        margin-top: 20px;
     }
 
-    .day-date {
-        font-size: 14px;
-        font-weight: normal;
-        margin-top: 8px;
+    .time-heading {
+        font-size: 20px;
+        margin-bottom: 10px;
     }
 
-    .status-container {
-        position: relative;
-        /* Add any other styles for your status container */
+    .time-remaining {
+        font-size: 30px;
     }
-
     .home-button {
         position: fixed;
         top: 20px;
