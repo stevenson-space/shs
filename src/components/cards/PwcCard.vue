6606540a86edc07a7b70fdb2ee76fc4d913c2bc5
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2023-08-07 10:50:37 -0500

    Changed closing visual to just say next open time instead of countdown

--- src/components/cards/PwcCard.vue
+++ src/components/cards/PwcCard.vue
@@ -1,156 +1,145 @@
 <template>
-    <card class="card">
+    <card v-if="showPWCSchedule" class="card">
         <p class="title">PWC Hours</p>
         <div class="green-line"></div>
         <div class="countdown">
-            <p v-if="isOpen">{{ countdownTime }}</p>
-            <p v-else>{{ countdownTime }}</p>
-            <p v-if="isOpen" class="time-text">Time remaining until closure</p>
-            <p v-else class="time-text">Time remaining until next opening</p>
+            <p>{{ openingStatus }}</p>
+            <p class="time-text">{{ timeStatus }}</p>
         </div>
     </card>
 </template>
 
 <script>
-    import Card from '@/components/Card.vue';
-
+import { mapState } from 'pinia';
+import Card from '@/components/Card.vue';
+import useUserSettingsStore from '@/stores/user-settings';
 
 export default {
+  components: { Card },
+  data() {
+    return {
+      openingTimes: [
+        { day: 1, start: '15:30', end: '20:00' }, // Monday
+        { day: 2, start: '15:30', end: '20:00' }, // Tuesday
+        { day: 3, start: '15:30', end: '20:00' }, // Wednesday
+        { day: 4, start: '15:30', end: '20:00' }, // Thursday
+        { day: 5, start: '15:30', end: '18:00' }, // Friday
+        { day: 6, start: '10:00', end: '15:00' }, // Saturday
+        { day: 0, start: '10:00', end: '15:00' }, // Sunday
+      ],
+      currentTimeMs: 0,
+    };
+  },
+  computed: {
+    ...mapState(useUserSettingsStore, ['showPWCSchedule']),
+    currentTime() {
+      return new Date(this.currentTimeMs);
+    },
+    currentDay() {
+      return this.currentTime.getDay();
+    },
+    currentOpeningTime() {
+      const openingTime = this.openingTimes.find(
+        (time) => time.day === this.currentDay,
+      );
+      return this.getTimeInMs(
+        this.currentTime.toDateString(),
+        openingTime.start,
+      );
+    },
+    currentClosingTime() {
+      const closingTime = this.openingTimes.find(
+        (time) => time.day === this.currentDay,
+      );
+      return this.getTimeInMs(
+        this.currentTime.toDateString(),
+        closingTime.end,
+      );
+    },
+    nextOpeningTime() {
+      if (this.currentTime < this.currentOpeningTime) {
+        const openingTime = this.openingTimes.find(
+          (time) => time.day === this.currentDay,
+        );
+        return this.getTimeInMs(
+          this.getNextDate(this.currentDay),
+          openingTime.start,
+        );
+      }
+      const nextDay = (this.currentDay + 1) % 7;
+      const openingTime = this.openingTimes.find(
+        (time) => time.day === nextDay,
+      );
+      return this.getTimeInMs(this.getNextDate(nextDay), openingTime.start);
+    },
+    isOpen() {
+      return (
+        this.currentTime >= this.currentOpeningTime
+                    && this.currentTime <= this.currentClosingTime
+      );
+    },
+    openingStatus() {
+      return this.isOpen ? 'Open' : `Opens at ${this.formatTime(this.nextOpeningTime)}`;
+    },
+    timeStatus() {
+      return this.isOpen ? 'Closes' : 'Closed';
+    },
+  },
+  methods: {
+    getTimeInMs(dateString, timeString) {
+      const dateTimeString = `${dateString} ${timeString}`;
+      return new Date(dateTimeString).getTime();
+    },
+    getNextDate(day) {
+      const today = this.currentTime.getDay();
+      const difference = (day - today + 7) % 7;
+      const nextDate = new Date(this.currentTime);
+      nextDate.setDate(this.currentTime.getDate() + difference);
+      return nextDate.toDateString();
+    },
+    formatTime(timeInMs) {
+      const date = new Date(timeInMs);
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const period = hours >= 12 ? 'pm' : 'am';
+      const formattedHours = hours % 12 || 12;
+      const formattedMinutes = String(minutes).padStart(2, '0');
+      return `${formattedHours}:${formattedMinutes} ${period}`;
+    },
+    updateCurrentTime() {
+      this.currentTimeMs = Date.now();
+    },
+    goToHomepage() {
+      // Use Vue Router to navigate to the homepage
+      this.$router.push('/');
+    },
+  },
+  created() {
+    // Initialize countdown immediately
+    this.updateCurrentTime();
 
-        components: { Card },components: { Card },
-        data() {
-            return {
-                openingTimes: [
-                    { day: 1, start: "15:30", end: "20:00" }, // Monday
-                    { day: 2, start: "15:30", end: "20:00" }, // Tuesday
-                    { day: 3, start: "15:30", end: "20:00" }, // Wednesday
-                    { day: 4, start: "15:30", end: "20:00" }, // Thursday
-                    { day: 5, start: "15:30", end: "18:00" }, // Friday
-                    { day: 6, start: "10:00", end: "15:00" }, // Saturday
-                    { day: 0, start: "10:00", end: "15:00" }, // Sunday
-                ],
-                currentTimeMs: 0,
-            };
-        },
-        computed: {
-            currentTime() {
-                return new Date(this.currentTimeMs);
-            },
-            currentDay() {
-                return this.currentTime.getDay();
-            },
-            currentOpeningTime() {
-                const openingTime = this.openingTimes.find(
-                    (time) => time.day === this.currentDay
-                );
-                return this.getTimeInMs(
-                    this.currentTime.toDateString(),
-                    openingTime.start
-                );
-            },
-            currentClosingTime() {
-                const closingTime = this.openingTimes.find(
-                    (time) => time.day === this.currentDay
-                );
-                return this.getTimeInMs(
-                    this.currentTime.toDateString(),
-                    closingTime.end
-                );
-            },
-            nextOpeningTime() {
-                if (this.currentTime < this.currentOpeningTime) {
-                    const openingTime = this.openingTimes.find(
-                        (time) => time.day === this.currentDay
-                    );
-                    return this.getTimeInMs(
-                        this.getNextDate(this.currentDay),
-                        openingTime.start
-                    );
-                } else {
-                    const nextDay = (this.currentDay + 1) % 7;
-                    const openingTime = this.openingTimes.find(
-                        (time) => time.day === nextDay
-                    );
-                    return this.getTimeInMs(this.getNextDate(nextDay), openingTime.start);
-                }
-            },
-            isOpen() {
-                return (
-                    this.currentTime >= this.currentOpeningTime &&
-                    this.currentTime <= this.currentClosingTime
-                );
-            },
-            countdownTime() {
-                const targetTime = this.isOpen
-                    ? this.currentClosingTime
-                    : this.nextOpeningTime;
-                const timeDiff = targetTime - this.currentTimeMs;
-                if (timeDiff <= 0) {
-                    return "00:00:00";
-                }
-                const hours = String(Math.floor(timeDiff / (1000 * 60 * 60))).padStart(
-                    2,
-                    "0"
-                );
-                const minutes = String(
-                    Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60))
-                ).padStart(2, "0");
-                const seconds = String(
-                    Math.floor((timeDiff % (1000 * 60)) / 1000)
-                ).padStart(2, "0");
-                return `${hours}:${minutes}:${seconds}`;
-            },
-        },
-        methods: {
-            getTimeInMs(dateString, timeString) {
-                const dateTimeString = `${dateString} ${timeString}`;
-                return new Date(dateTimeString).getTime();
-            },
-            getNextDate(day) {
-                const today = this.currentTime.getDay();
-                const difference = (day - today + 7) % 7;
-                const nextDate = new Date(this.currentTime);
-                nextDate.setDate(this.currentTime.getDate() + difference);
-                return nextDate.toDateString();
-            },
-            updateCurrentTime() {
-                this.currentTimeMs = Date.now();
-            },
-            goToHomepage() {
-                // Use Vue Router to navigate to the homepage
-                this.$router.push("/");
-            },
-        },
-        created() {
-            // Initialize countdown immediately
-            this.updateCurrentTime();
-
-            // Update currentTime every second
-            this.timer = setInterval(() => {
-                this.updateCurrentTime();
-            }, 1000);
-        },
-        beforeDestroy() {
-            clearInterval(this.timer);
-        },
-    };</script>
+    // Update currentTime every second
+    this.timer = setInterval(() => {
+      this.updateCurrentTime();
+    }, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
+  },
+};
+</script>
 
 <style lang="sass" scoped>
 @import 'src/styles/style.sass'
 
-
 .card
   padding: 0 8px
   padding-bottom: 5px
-  
-
 
 .title
     margin: 15px 0
     text-align: center
     font-size: 20px
-    
-
 
 .countdown
   font-size: 20px
@@ -161,16 +150,15 @@ export default {
   margin: 5px 0
     text-align: center
 
-.time-text 
-  font-size: 12px 
+.time-text
+  font-size: 12px
   text-align: center
   color: #333
 
-.green-line 
+.green-line
   width: 100%
   height: 1px
   background-color: #006400
   margin-bottom: 10px
-
 
 </style>
--- src/stores/user-settings.ts
+++ src/stores/user-settings.ts
@@ -2,11 +2,13 @@ import { defineStore } from 'pinia';
 
 interface State {
   grade: string
+  showPWCSchedule: boolean
 }
 
 export default defineStore('grades', {
   state: (): State => ({
     grade: 'None',
+    showPWCSchedule: false,
   }),
   actions: {
     initializeGrade(): void {
@@ -14,9 +16,18 @@ export default defineStore('grades', {
         this.setGrade(localStorage.grade);
       }
     },
+    initializeShowPWCSchedule(): void {
+      if (localStorage.showPWCSchedule) {
+        this.setShowPWCSchedule(localStorage.showPWCSchedule === 'true');
+      }
+    },
     setGrade(grade: string): void {
       this.grade = grade;
       localStorage.grade = grade;
+    },
+    setShowPWCSchedule(showPWCSchedule: boolean): void {
+      this.showPWCSchedule = showPWCSchedule;
+      localStorage.showPWCSchedule = showPWCSchedule;
     },
   },
 });
