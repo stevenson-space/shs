9d868c8f64266f8bacc50241b379ec110b768a2a
andrew <me@andreww.co>
2024-01-26 16:32:31 -0600

    fix live redirect

--- src/views/Live/Live.vue
+++ src/views/Live/Live.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
   mounted() {
-    window.location.href = 'https://youtube.com/@stevensonpatriotathletics6955?/live';
+    window.location.href = 'https://youtube.com/@stevensonpatriotathletics6955/live';
   },
 };
 </script>
