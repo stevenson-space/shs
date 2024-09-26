72d00d55d557864386c2f15dbdb9b697513df9cb
andrew <me@andreww.co>
2024-09-25 21:32:15 -0500

    add new links

---
+++ src/views/Apply/Apply.vue
@@ -0,0 +1,7 @@
+<script>
+export default {
+  mounted() {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSc3orRU-tOxm6tHaP3v2PqW7nYtyqG59enV82ZYZRf-HPe-Yg/viewform?usp=sf_link';
+  },
+};
+</script>
---
+++ src/views/Snowball/Snowball.vue
@@ -0,0 +1,7 @@
+<script>
+export default {
+  mounted() {
+    window.location.href = 'https://omella.com/4o5zs';
+  },
+};
+</script>
