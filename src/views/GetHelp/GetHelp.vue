af5dc2e4e822cd5a401809689c6780ca9c175271
andrew <me@andreww.co>
2024-09-26 21:07:14 -0500

    update help form to personal acct

--- src/views/GetHelp/GetHelp.vue
+++ src/views/GetHelp/GetHelp.vue
@@ -3,10 +3,10 @@ export default {
   mounted() {
     // stops an extra SS page from opening when we refer to this link on-site
     if (!new URLSearchParams(window.location.search).has('referrer')) {
-      window.open('https://docs.google.com/forms/d/e/1FAIpQLScPaVlcJ0eWdt6N1WQ7FDLambjHbg8PWZml_SqAmxtAY0EvJg/viewform');
+      window.open('https://docs.google.com/forms/d/e/1FAIpQLSeCfuWhQ5f4jl0bzcEGJ6m6BGpnRkKJFEUW10knH0Olic_XAQ/viewform?usp=sf_link');
       this.$router.push('/');
     } else {
-      window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScPaVlcJ0eWdt6N1WQ7FDLambjHbg8PWZml_SqAmxtAY0EvJg/viewform';
+      window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSeCfuWhQ5f4jl0bzcEGJ6m6BGpnRkKJFEUW10knH0Olic_XAQ/viewform?usp=sf_link';
     }
   },
 };
