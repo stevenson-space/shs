33ad73ae36791fb381c9e149c34a1d68a47b10d9
Joshua Fernandes <158479286+joshuaf745@users.noreply.github.com>
2025-05-11 07:10:44 -0500

    Update to show 2025 Activist Songwriting Playlist

--- src/components/cards/NewFeatureCard.vue
+++ src/components/cards/NewFeatureCard.vue
@@ -1,9 +1,25 @@
 <template>
-  <timed-card startTime="March 6, 2025" endTime="March 8, 2025">
+  <timed-card startTime="May 8, 2025" endTime="May 26, 2025">
     <div class="container">
       <div class="title"></div>
       <div class="message">
-        <a class="link" href="/snowblitz" target="_blank">❄️ Operation Snowball's Snowblitz is TODAY!</a> Don't miss out on a night of fun activities, team building, bonding with friends, and pizza! 
+        SHS's 2024-25 Music Production 2 classes engaged in an activist songwriting project centered around important topics to raise awareness for
+        <a class="link" href="https://www.gkmcenglewood.com/" target="_blank">Good Kids Mad City</a>,
+        <a class="link" href="https://nami-nwsc.org/" target="_blank">NAMI Northwest Suburbs Chicago</a>, and
+        <a class="link" href="https://buffalogrovepride.com/" target="_blank">The Pinta Pride Project</a>!
+
+        <div class= "chips">
+          <LinkChip
+            href="https://youtube.com/playlist?list=PLap5ZkjM9rSEGwfnayinyap-wGVZNdN8g&si=jkeY3i9zYnPJueEx"
+            label="Listen Now!"
+            background="#fbe9e7"
+          />
+          <LinkChip
+            href="https://omella.com/9zh2h"
+            label="Donate Here"
+            background="#dcffdb"
+          />
+        </div>
       </div>
     </div>
   </timed-card>
@@ -11,9 +27,10 @@
 
 <script>
 import TimedCard from "@/components/cards/TimedCard.vue";
+import LinkChip from "@/components/cards/LinkChip.vue";
 
 export default {
-  components: { TimedCard },
+  components: { TimedCard, LinkChip },
 };
 </script>
 
@@ -45,4 +62,11 @@ a
     margin: 0 20px
     line-height: 1.6em
     text-align: center
+.chips
+  display: flex
+  gap: 10px
+  flex-wrap: wrap
+  justify-content: center
+  margin-top: 8px
+
 </style>
