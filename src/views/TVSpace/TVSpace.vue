2c0007de2300482b3eadd6a3de52e8b05c5bfc86
Joshua Fernandes <158479286+joshuaf745@users.noreply.github.com>
2025-08-06 22:20:10 -0500

    Create very basic page for display on TV

---
+++ src/views/TVSpace/TVSpace.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="page">
+    <Card>
+      <div class="container">
+        <span class="countdown">
+          {{ intoCountdownString(this.totalSecondsLeft) }}
+        </span>
+        <h6>{{ this.bell.type.toUpperCase() }}</h6>
+      </div>
+    </Card>
+    <ScheduleCard title=""/>
+  </div>
+</template>
+
+<script lang="ts">
+import ScheduleCard from '@/components/cards/ScheduleCard.vue';
+import useClockStore from '@/stores/clock'
+import Card from "@/components/Card.vue";
+import { intoCountdownString } from '@/utils/countdown';
+import { mapState } from "pinia";
+import {dateToSeconds} from "@/utils/util";
+
+export default {
+  components: {
+    ScheduleCard,
+    Card
+  },
+  computed: {
+    ...mapState(useClockStore, ['bell', 'date']),
+    totalSecondsLeft() {
+      return this.bell.getSecondsUntilNextTarget() - dateToSeconds(this.date)
+    }
+  },
+  methods: {
+    intoCountdownString,
+  }
+}
+</script>
+
+<style scoped lang="sass">
+@import '@/styles/style.sass'
+
+.page
+  height: 100vh
+  display: flex
+  flex-direction: column
+  justify-content: flex-start
+  overflow: hidden
+
+.container
+  display: flex
+  flex-direction: column
+  align-items: center
+  gap: 2px
+  padding: 6px
+  background: var(--background)
+  color: var(--secondary)
+  border-radius: 15px
+  font-weight: bold
+
+  .countdown
+    font-size: 3em
+    line-height: 1em
+    color: var(--secondary)
+    margin-top: 8px
+
+  h6
+    color: darkgrey
+    font-size: large
+    padding-bottom: 8px
+    margin: 8px
+</style>
