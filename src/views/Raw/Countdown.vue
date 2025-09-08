477d8a71144f96a037f87f056a17183ab45966c8
Joshua Fernandes <158479286+joshuaf745@users.noreply.github.com>
2025-09-08 07:01:24 -0500

    Make /raw/countdown scale to be bigger

--- src/views/Raw/Countdown.vue
+++ src/views/Raw/Countdown.vue
@@ -46,23 +46,38 @@ export default {
   background: var(--background)
   color: var(--secondary)
   font-family: 'Open Sans', Helvetica, sans-serif
+  padding: 2rem
+  box-sizing: border-box
 
 .countdown
-  font-size: 4rem
+  font-size: min(12vw, 12rem)
   font-weight: bold
   color: var(--secondary)
-  margin-bottom: 1rem
+  margin-bottom: min(3vw, 3rem)
+  line-height: 1em
+  text-align: center
+  +mobile-small
+    font-size: min(15vw, 4rem)
+    margin-bottom: min(4vw, 1.5rem)
 
 .schedule-name
-  font-size: 2rem
+  font-size: min(4vw, 4rem)
   font-weight: bold
   color: var(--secondary)
   text-transform: uppercase
+  text-align: center
+  +mobile-small
+    font-size: min(8vw, 2rem)
 
 .next-day
-  font-size: 1.5rem
+  font-size: min(4vw, 4rem)
   color: var(--secondary)
   text-align: center
   white-space: pre-line
   font-weight: normal
+  max-width: 80vw
+  line-height: 1.3
+  +mobile-small
+    font-size: min(5vw, 1.5rem)
+    max-width: 90vw
 </style>
