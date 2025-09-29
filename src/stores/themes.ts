ea0952ae2ec408391e06bea56a6d1ed99460d4bf
Joshua Fernandes <158479286+joshuaf745@users.noreply.github.com>
2025-09-28 21:31:48 -0500

    Temporarily refactor theme storage to test out new theme system

--- src/stores/themes.ts
+++ src/stores/themes.ts
@@ -2,55 +2,56 @@ import { defineStore } from 'pinia';
 import _themeIdeas from '@/data/themeIdeas.json';
 import _themes from '@/data/themes.json';
 import { set as GASet } from 'vue-gtag';
-import { ThemeData, Theme } from '@/utils/types';
-
-const themes: Theme[] = _themes;
-const themeIdeas: Theme[] = _themeIdeas;
+import lightTheme from '@/themes/base/light.json'
+import darkTheme from '@/themes/base/dark.json'
+import { Theme } from '@/utils/types';
 
 interface State {
-  color: string,
-  theme: object,
+  // color: string,
+  theme: Theme,
+  resolvedAssets: Map<string, string>,
 }
 
 export default defineStore('themes', {
   state: (): State => ({
-    color: import.meta.env.VITE_EDIT_COLORS === 'true' ? themeIdeas[themeIdeas.length - 1].suggestedColor : themes[0].suggestedColor,
-    theme: import.meta.env.VITE_EDIT_COLORS === 'true' ? themeIdeas[themeIdeas.length - 1] : themes[0],
+    // color: import.meta.env.VITE_EDIT_COLORS === 'true' ? themeIdeas[themeIdeas.length - 1].suggestedColor : themes[0].suggestedColor,
+    theme: /*import.meta.env.VITE_EDIT_COLORS === 'true' ? themeIdeas[themeIdeas.length - 1] : */lightTheme as Theme,
+    resolvedAssets: new Map(),
   }),
   actions: {
     initializeTheme(): void {
-      if (localStorage.color && import.meta.env.VITE_EDIT_COLORS !== 'true') {
-        this.setColor(localStorage.color);
-        GASet({ user_properties: {
-          color: localStorage.color,
-        } });
-      } else {
-        GASet({ user_properties: {
-          color: 'unset',
-        } });
-      }
+      // if (localStorage.color && import.meta.env.VITE_EDIT_COLORS !== 'true') {
+      //   this.setColor(localStorage.color);
+      //   GASet({ user_properties: {
+      //     color: localStorage.color,
+      //   } });
+      // } else {
+      //   GASet({ user_properties: {
+      //     color: 'unset',
+      //   } });
+      // }
       if (localStorage.theme && import.meta.env.VITE_EDIT_COLORS !== 'true') {
-        const data:ThemeData = { theme: JSON.parse(localStorage.theme), useThemeColor: false };
-        this.setTheme(data);
+        this.setTheme(JSON.parse(localStorage.theme));
+      } else {
+        this.setTheme(lightTheme as Theme);
       }
     },
     setColor(color:string): void {
-      this.color = color;
-      localStorage.color = color;
-      GASet({ user_properties: {
-        color,
-      } });
+    //   this.color = color;
+    //   localStorage.color = color;
+    //   GASet({ user_properties: {
+    //     color,
+    //   } });
     },
-    setTheme(data: ThemeData): void {
-      const { useThemeColor, theme } = data;
-      const color = theme.suggestedColor;
-      if (useThemeColor) {
-        this.color = color;
-        localStorage.color = color;
-        GASet({ user_properties: {
-          color,
-        } });
-      }
+    setTheme(theme: Theme): void {
+      // const color = theme.suggestedColor;
+      // if (useThemeColor) {
+      //   this.color = color;
+      //   localStorage.color = color;
+      //   GASet({ user_properties: {
+      //     color,
+      //   } });
+      // }
       this.theme = theme;
       localStorage.theme = JSON.stringify(theme);
     },
