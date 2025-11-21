3f453b3f23725c9c01ed3f42ef00794f27eae9c9
Joshua Fernandes <158479286+joshuaf745@users.noreply.github.com>
2025-11-20 19:17:41 -0600

    Switch button order

--- src/views/Theme/Theme.vue
+++ src/views/Theme/Theme.vue
@@ -125,18 +125,6 @@
                   </info-tooltip>
                   <info-tooltip>
                     <template #trigger>
-                      <icon-button @click="exportTheme">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                          <polyline points="7 10 12 15 17 10"></polyline>
-                          <line x1="12" y1="15" x2="12" y2="3"></line>
-                        </svg>
-                      </icon-button>
-                    </template>
-                    Export Theme
-                  </info-tooltip>
-                  <info-tooltip>
-                    <template #trigger>
                       <icon-button :disabled="true">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                           <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path>
@@ -146,6 +134,18 @@
                       </icon-button>
                     </template>
                     Save Theme
+                  </info-tooltip>
+                  <info-tooltip>
+                    <template #trigger>
+                      <icon-button @click="exportTheme">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                          <polyline points="7 10 12 15 17 10"></polyline>
+                          <line x1="12" y1="15" x2="12" y2="3"></line>
+                        </svg>
+                      </icon-button>
+                    </template>
+                    Export Theme
                   </info-tooltip>
                 </div>
               </div>
