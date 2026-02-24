3b6f4a8ba5ac66c777fda1d1d0ec025c34bade3d
Joshua <158479286+joshuaf745@users.noreply.github.com>
2026-02-23 21:02:10 -0600

    Use new pinia store format for schedules

--- src/stores/schedules.ts
+++ src/stores/schedules.ts
@@ -1,106 +1,105 @@
 import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
 import { RouteLocationNormalized } from 'vue-router';
 import officialSchedules from '@/data/schedules.json';
 import { CustomSchedules, Schedule, ScheduleCollection } from '@/utils/types';
 import { tryParseJSON, getNameWithoutConflicts } from '@/utils/util';
 import Bell from '@/utils/bell';
 
-interface State {
-  customSchedules: CustomSchedules;
-  defaultScheduleMode: string;
-  scheduleMode: string;
-}
+export default defineStore('schedules', () => {
+  const customSchedules = ref<CustomSchedules>({} as CustomSchedules);
+  const defaultScheduleMode = ref('Normal');
+  const scheduleMode = ref('');
 
-export default defineStore('schedules', {
-  // convert to a function
-  state: (): State => ({
-    customSchedules: {} as CustomSchedules,
-    defaultScheduleMode: 'Normal',
-    scheduleMode: '',
-  }),
-  getters: {
-    schedules(state): ScheduleCollection[] { // we merge the officialSchedules provided by us and the user defined customSchedules
-      const schedules: ScheduleCollection[] = JSON.parse(JSON.stringify(officialSchedules)); // begin with copying the officialSchedules
-      // customSchedules is an object with `schedule type` keys and values containing a
-      // list of schedule mode objects for the corresponding schedule type
-      const { customSchedules } = state;
-      if (customSchedules) {
-        for (const schedule of schedules) {
-          const customScheduleModes = customSchedules[schedule.name];
-          if (customScheduleModes) { // if there exist custom schedules for the current schedule type
-            for (const scheduleMode of customScheduleModes) {
-              // add the scheduleMode, and also include an `isCustom` flag to later be able to distinguish them
-              schedule.modes.push({ ...scheduleMode, isCustom: true });
-            }
+  // we merge the officialSchedules provided by us and the user defined customSchedules
+  const schedules = computed((): ScheduleCollection[] => {
+    const scheduleList: ScheduleCollection[] = JSON.parse(JSON.stringify(officialSchedules)); // begin with copying the officialSchedules
+    // customSchedules is an object with `schedule type` keys and values containing a
+    // list of schedule mode objects for the corresponding schedule type
+    const custom = customSchedules.value;
+    if (custom) {
+      for (const schedule of scheduleList) {
+        const customScheduleModes = custom[schedule.name];
+        if (customScheduleModes) { // if there exist custom schedules for the current schedule type
+          for (const scheduleMode of customScheduleModes) {
+            // add the scheduleMode, and also include an `isCustom` flag to later be able to distinguish them
+            schedule.modes.push({ ...scheduleMode, isCustom: true });
           }
         }
       }
-      return schedules;
-    },
-  },
-  actions: {
-    initializeSchedule(): void {
-      this.setCustomSchedules(tryParseJSON(localStorage.customSchedules) as CustomSchedules);
-      if (localStorage.defaultScheduleMode) {
-        this.setDefaultScheduleMode(localStorage.defaultScheduleMode);
-        this.setScheduleMode(localStorage.defaultScheduleMode);
-      }
-    },
-    addCustomScheduleMode({ scheduleType, scheduleToAdd, scheduleToReplace }: { scheduleType: string, scheduleToAdd: { start: string[], end: string[], name: string, periods: string }, scheduleToReplace: string }): void {
-      // If scheduleToReplace is not defined, then we want to just add to end of scheduleModes list
-      const scheduleModes = (this.customSchedules || {})[scheduleType] || [] as CustomSchedules[];
-      const replaceIndex = scheduleModes.map((mode: CustomSchedules) => mode.name).indexOf(scheduleToReplace);
-      // Notice that scheduleModes only contains schedules from customSchedules, so if scheduleToReplace is an
-      // official schedule, then replaceIndex will be -1, and we'll just add the new schedule without replacing anything
-      // (this is the expected behavior because official schedules can't be modified or replaced)
-      if (replaceIndex !== -1) {
-        scheduleModes.splice(replaceIndex, 1, scheduleToAdd);
-      } else {
-        scheduleModes.push(scheduleToAdd);
-      }
-      this.setCustomSchedules({ ...this.customSchedules, [scheduleType]: scheduleModes });
-    },
-    removeCustomScheduleMode({ scheduleType, scheduleToRemove }: { scheduleType: string, scheduleToRemove: string}): void {
-      // if scheduleType is not provided, then we remove scheduleToRemove for all schedule types
-      for (const [type, scheduleModes] of Object.entries(this.customSchedules || {}) as [any, Schedule[]]) {
-        if (!scheduleType || scheduleType === type) {
-          const removeIndex = scheduleModes.map((mode: Schedule) => mode.name).indexOf(scheduleToRemove);
-          if (removeIndex > -1) {
-            scheduleModes.splice(removeIndex, 1);
-          }
+    }
+    return scheduleList;
+  });
+
+  function initializeSchedule(): void {
+    setCustomSchedules(tryParseJSON(localStorage.customSchedules) as CustomSchedules);
+    if (localStorage.defaultScheduleMode) {
+      setDefaultScheduleMode(localStorage.defaultScheduleMode);
+      setScheduleMode(localStorage.defaultScheduleMode);
+    }
+  }
+
+  function addCustomScheduleMode({ scheduleType, scheduleToAdd, scheduleToReplace }: { scheduleType: string, scheduleToAdd: { start: string[], end: string[], name: string, periods: string }, scheduleToReplace: string }): void {
+    // If scheduleToReplace is not defined, then we want to just add to end of scheduleModes list
+    const scheduleModes = (customSchedules.value || {})[scheduleType] || [] as CustomSchedules[];
+    const replaceIndex = scheduleModes.map((mode: CustomSchedules) => mode.name).indexOf(scheduleToReplace);
+    // Notice that scheduleModes only contains schedules from customSchedules, so if scheduleToReplace is an
+    // official schedule, then replaceIndex will be -1, and we'll just add the new schedule without replacing anything
+    // (this is the expected behavior because official schedules can't be modified or replaced)
+    if (replaceIndex !== -1) {
+      scheduleModes.splice(replaceIndex, 1, scheduleToAdd);
+    } else {
+      scheduleModes.push(scheduleToAdd);
+    }
+    setCustomSchedules({ ...customSchedules.value, [scheduleType]: scheduleModes });
+  }
+
+  function removeCustomScheduleMode({ scheduleType, scheduleToRemove }: { scheduleType: string, scheduleToRemove: string}): void {
+    // if scheduleType is not provided, then we remove scheduleToRemove for all schedule types
+    for (const [type, scheduleModes] of Object.entries(customSchedules.value || {}) as [any, Schedule[]]) {
+      if (!scheduleType || scheduleType === type) {
+        const removeIndex = scheduleModes.map((mode: Schedule) => mode.name).indexOf(scheduleToRemove);
+        if (removeIndex > -1) {
+          scheduleModes.splice(removeIndex, 1);
         }
       }
-      this.setCustomSchedules(this.customSchedules);
-    },
-    setCustomSchedules(schedules: CustomSchedules): void {
-      // If there are any schedule modes with the same name as an official schedule mode, we want to rename the custom one
-      const officalScheduleModeNames = new Set();
-      for (const schedule of officialSchedules) {
-        for (const scheduleMode of schedule.modes) {
-          officalScheduleModeNames.add(scheduleMode.name);
-        }
+    }
+    setCustomSchedules(customSchedules.value);
+  }
+
+  function setCustomSchedules(value: CustomSchedules): void {
+    // If there are any schedule modes with the same name as an official schedule mode, we want to rename the custom one
+    const officalScheduleModeNames = new Set();
+    for (const schedule of officialSchedules) {
+      for (const scheduleMode of schedule.modes) {
+        officalScheduleModeNames.add(scheduleMode.name);
       }
-      for (const scheduleModes of Object.values(schedules)) {
-        for (const scheduleMode of scheduleModes) {
-          scheduleMode.name = getNameWithoutConflicts(
-            scheduleMode.name,
-            (name) => officalScheduleModeNames.has(name),
-          );
-        }
+    }
+    for (const scheduleModes of Object.values(value)) {
+      for (const scheduleMode of scheduleModes) {
+        scheduleMode.name = getNameWithoutConflicts(
+          scheduleMode.name,
+          (name) => officalScheduleModeNames.has(name),
+        );
       }
-      this.customSchedules = schedules;
-      localStorage.customSchedules = JSON.stringify(schedules);
-    },
-    resetSchedules(): void {
-      this.customSchedules = {};
-      localStorage.customSchedules = {};
-    },
-    setDefaultScheduleMode(schedule: string): void {
-      this.defaultScheduleMode = schedule;
-      localStorage.defaultScheduleMode = schedule;
-    },
-    setScheduleMode(scheduleMode: string): void {
-      this.scheduleMode = scheduleMode;
-    },
-  },
+    }
+    customSchedules.value = value;
+    localStorage.customSchedules = JSON.stringify(value);
+  }
+
+  function resetSchedules(): void {
+    customSchedules.value = {} as CustomSchedules;
+    localStorage.customSchedules = {};
+  }
+
+  function setDefaultScheduleMode(value: string): void {
+    defaultScheduleMode.value = value;
+    localStorage.defaultScheduleMode = value;
+  }
+
+  function setScheduleMode(value: string): void {
+    scheduleMode.value = value;
+  }
+
+  return { customSchedules, defaultScheduleMode, scheduleMode, schedules, initializeSchedule, addCustomScheduleMode, removeCustomScheduleMode, setCustomSchedules, resetSchedules, setDefaultScheduleMode, setScheduleMode };
 });
