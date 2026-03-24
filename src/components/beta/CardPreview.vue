af127fbf3a05d0ec7a8afe92f132a9b901fa8544
Joshua Fernandes <158479286+joshuaf745@users.noreply.github.com>
2026-03-24 14:17:41 -0500

    Create card preview to display cards properly in storybook

---
+++ src/components/beta/CardPreview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card-preview">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@reference "@/styles/tailwind.css";
+
+.card-preview {
+  width: 300px;
+  background: #f5f5f0;
+  padding: 18px 14px 24px;
+  border-radius: 14px;
+}
+</style>
