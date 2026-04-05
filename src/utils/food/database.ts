104a047a69f9ad594c5d4d9255824d4677562f65
Joshua <158479286+joshuaf745@users.noreply.github.com>
2026-04-05 12:55:56 -0500

    Create method to build nutritional database

--- src/utils/food/database.ts
+++ src/utils/food/database.ts
@@ -1,4 +1,18 @@
-import type { FoodInformation, MenuItem } from "./types";
+import { FoodInformation, MenuItem } from "./types";
 
 export type NutritionalDatabase = Record<string, FoodInformation>;
 export type MenuDatabase = MenuItem[];
+
+type EagerComponentModules = Record<string, { default: FoodInformation[] }>;
+
+export function buildNutritionalDatabase(modules: EagerComponentModules): NutritionalDatabase {
+  const db: NutritionalDatabase = {};
+
+  for (const { default: items } of Object.values(modules)) {
+    for (const item of items) {
+      db[item.metadata.name] = item;
+    }
+  }
+
+  return db;
+}
