21f6078ec50141c8fb3e93d1701b100a26b2ed12
Joshua <158479286+joshuaf745@users.noreply.github.com>
2026-04-19 16:49:51 -0500

    Create type for definitions of weekly menu

--- src/utils/food/auto-lunch.ts
+++ src/utils/food/auto-lunch.ts
@@ -8,4 +8,16 @@ export const DayMenu = z.object({
   international: z.string(),
 });
 
+const StationItems = z.union([
+  z.tuple([z.string()]),
+  z.tuple([z.string(), z.string()]),
+]);
+
+export const WeekEntries = z.discriminatedUnion("cadence", [
+  // not sure if StationItems union is the best way to handle this
+  z.object({ cadence: z.literal("weekly"), data: StationItems }),
+  z.object({ cadence: z.literal("daily"), data: z.array(StationItems).length(5) }),
+]);
+
 export type DayMenu = z.infer<typeof DayMenu>;
+export type WeekEntries = z.infer<typeof WeekEntries>;
