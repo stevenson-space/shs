68638f616f3f1b1b80938699398e0ee5bb2e19cc
Joshua <158479286+joshuaf745@users.noreply.github.com>
2026-04-19 10:27:41 -0500

    Support older browsers

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
       injectRegister: false,
       registerType: 'autoUpdate',
       injectManifest: {
+        rollupFormat: 'iife',
         globPatterns: ['**/*.{js,css,html,svg,png,ico,woff2,mp3}'],
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
