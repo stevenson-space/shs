8f44c3d700642053c5df928b599f876038eb6357
Joshua <158479286+joshuaf745@users.noreply.github.com>
2026-04-19 19:35:53 -0500

    Create function to get day menu for a specific date

--- src/utils/food/rotating-map.ts
+++ src/utils/food/rotating-map.ts
@@ -1,21 +1,28 @@
-import { RotatingStation, WeeklyEntries } from "./rotating-schema";
+import { DayMenu, RotatingStation, WeeklyEntries, SpecialStationEntries } from "./rotating-schema";
 
 import comfort from "../../data/lunch-rotating/comfort.json";
 import mindful from "../../data/lunch-rotating/mindful.json";
 import sides from "../../data/lunch-rotating/sides.json";
 import soup from "../../data/lunch-rotating/soup.json";
 import international from "../../data/lunch-rotating/international.json";
+import special from "../../data/lunch-rotating/special.json";
 
 export class RotatingMenuMap {
   validFrom: Date
   validTo: Date
+  semesterSwitch: Date
   // which week in the lunch menu does 'validFrom' correspond to?
   offset: number
   stations: Record<RotatingStation, WeeklyEntries>
+  special: SpecialStationEntries
 
-  constructor(validFrom: Date, validTo: Date, offset: number, stations: Record<RotatingStation, unknown>) {
+  constructor(validFrom: Date, validTo: Date, semesterSwitch: Date, offset: number, stations: Record<RotatingStation, unknown>, special: unknown) {
     if (validFrom >= validTo) {
       throw new RangeError("validFrom must be before validTo");
+    }
+
+    if (validFrom > semesterSwitch || semesterSwitch > validFrom) {
+      throw new RangeError("semesterSwitch must be a valid date");
     }
 
     if (0 > offset || offset >= 4) {
@@ -24,17 +31,54 @@ export class RotatingMenuMap {
 
     this.validFrom = validFrom;
     this.validTo = validTo;
+    this.semesterSwitch = semesterSwitch;
     this.offset = offset;
 
     this.stations = Object.fromEntries(
       RotatingStation.options.map(key => [key, WeeklyEntries.parse(stations[key])])
     ) as Record<RotatingStation, WeeklyEntries>;
+
+    this.special = SpecialStationEntries.parse(special);
+  }
+
+  private weeksSince(date: Date) {
+    return Math.floor((date.getTime() - this.validFrom.getTime()) / (7 * 24 * 60 * 60 * 1000));
+  }
+
+  private currentWeekIndex(date: Date) {
+    return (this.weeksSince(date) + this.offset) % 4;
+  }
+
+  getMenuUnchecked(date: Date): DayMenu {
+    if (this.validFrom > date || date > this.validFrom) {
+      throw new RangeError("date must be a valid date");
+    }
+
+    const week = this.currentWeekIndex(date);
+    const day = (date.getDay() + 6) % 7; // mon = 0, fri = 4
+
+    return {
+      ...Object.fromEntries(
+        RotatingStation.options.map(key => {
+          const entry = this.stations[key];
+          const item = entry.cadence === "weekly"
+            ? entry.data[week]
+            : entry.data[week][day];
+          return [key, item];
+        })
+      ),
+      special: (date < this.semesterSwitch)
+        ? this.special[0][day]
+        : this.special[1][day],
+    } as DayMenu;
   }
 }
 
 export const rotatingMenuMap = new RotatingMenuMap(
   new Date("2025-08-11"),
   new Date("2026-05-22"),
+  new Date("2026-01-01"),
   0,
   { comfort, mindful, sides, soup, international },
+  special,
 );
